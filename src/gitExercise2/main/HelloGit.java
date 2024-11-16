5508d31b34947c8f1a7a5f457dcff00eced34b7b
Roo55 <anonacc32@gmail.com>
2024-11-16 19:44:49 +0100

    Sono il secondo developer e ho modificato una riga di codice

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -3,7 +3,7 @@ package gitExercise2.main;
 public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("Hello git!!!");
-		System.out.println("Questa è una stampa!");
+		System.out.println("Questa è una nuova stampa!");
 
 	}
 }
